2763f7837f654c24911ca4e361abfe77110ea2ea
EmiliaDai <daiyao0809@126.com>
2019-04-13 14:54:18 +0800

    dai

--- web/pc/src/vShop/commodity/Manage/comp/shopCard/index.vue
+++ web/pc/src/vShop/commodity/Manage/comp/shopCard/index.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-      rrr
+
+    <div class="shopcarStyle">
+      <Card>
+
+
+        <!--<div slot="title">-->
+          <!--<img :src="shopImg" alt="">-->
+        <!--</div>-->
+        <div class="ivu-card-body-photo box_row">
+          <div class="shopimg">
+            <img :src="shopImg" alt="">
+          </div>
+          <div class="shoptxt box_row_1auto">
+            iphone XR 128G
+
+          </div>
+          <div class="shoptxt box_row_1auto">
+            <div>品牌: Apple/苹果</div>
+            <div>内存： 128G</div>
+            <div>颜色：白色</div>
+
+          </div>
+          <div class="shoptxt box_row_1auto">
+            9990
+          </div>
+          <div class="shoptxt box_row_1auto">
+            13812340000
+          </div>
+          <div class="shoptxt box_row_1auto">
+            <div>2019-04-20</div>
+            <div>19：00</div>
+          </div>
+          <div class="shoptxt box_row_1auto">
+            <div>2019-04-21</div>
+            <div>19：00</div>
+          </div>
+
+
+        </div>
+      </Card>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+  import shopImg from '@/assets/images/login-bg.jpg'
+  export default {
+    name: "index",
+    data(){
+      return {
+        shopImg
+      }
     }
+
+  }
 </script>
 
-<style scoped>
+<style lang="less">
+ @import "./index.less";
 
 </style>
--- web/pc/src/vShop/commodity/Manage/index.vue
+++ web/pc/src/vShop/commodity/Manage/index.vue
@@ -1,8 +1,36 @@
 <template>
-      <div>
+      <div class="shopManager box_col" >
+        <div class="barShow" >
+          商品管理
+        </div>
+        <div class="box_row" style="padding: 0 16px">
+          <div class="titleBarShow" style="width: 100px;padding: 0px 5px;" >
+            商品图片
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            商品名称
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            商品详情
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            参与人数
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            中奖ID
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            开始时间
+          </div>
+          <div class="titleBarShow box_row_1auto" >
+            结束时间
+          </div>
 
-        商品管理
-        <shop-card v-for="(it,index) in 10"></shop-card>
+        </div>
+        <div class="box_col_autoY" style="background-color: #eeeeee">
+          <shop-card v-for="(it,index) in 3"></shop-card>
+        </div>
+
 
       </div>
 </template>
@@ -20,6 +48,20 @@
     }
 </script>
 
-<style scoped>
+<style lang="less">
+  .shopManager{
+    background-color: #eeeeee;
+    .barShow{
+      background-color: #dcdcdc;
+      height: 20px;
+    }
+    .titleBarShow{
+      padding: 0 10px;
+      text-align: center;
+      height: 20px;
+
+    }
+
+  }
 
 </style>
