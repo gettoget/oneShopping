3204804100ff53c03e55fa9d78af8dfc1731267d
zhaohu <965567396@qq.com>
2019-04-16 22:22:07 +0800

    z

--- web/pc/src/vShop/commodity/NewStock/index.vue
+++ web/pc/src/vShop/commodity/NewStock/index.vue
@@ -3,21 +3,14 @@
     <div>
       商品上架
     </div>
-    <cropper
-      :src="exampleImageSrc"
-      crop-button-text="确认提交"
-      @on-crop="handleCroped"
-    ></cropper>
   </div>
 </template>
 
 <script>
-  import Cropper from '@/components/cropper'
 
   export default {
     name: "index",
     components: {
-      Cropper
     },
     data () {
       return {
