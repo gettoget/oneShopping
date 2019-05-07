1d75de88ae7148686d940f4ba0f1435c24206d70
youaretheappleinmyeyes <965567396@qq.com>
2019-05-07 23:41:02 +0800

    zhaohu

--- web/pc/src/vShop/commodity/Manage/index.vue
+++ web/pc/src/vShop/commodity/Manage/index.vue
@@ -1,76 +1,66 @@
 <template>
-      <div class="shopManager box_col" >
-        <div style="padding:0 0 10px 0;font-size: 1.2rem; border-bottom:solid 1px #ccc;color: #666;" >
-          <Icon type="md-cube" color="#666" size="30"/>
-
-          商品管理
-        </div>
-        <div class="box_row barShow" style="padding: 11px 16px;border-bottom:solid 1px #ccc;">
-          <div class="titleBarShow" style="width: 100px;padding: 0px 5px;" >
-            商品图片
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            商品名称
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            商品详情
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            参与人数
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            中奖ID
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            开始时间
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            结束时间
-          </div>
-          <div class="titleBarShow box_row_1auto" >
-            商品状态
-          </div>
-
-        </div>
-        <div class="box_col_autoY" >
-          <shop-card v-for="(it,index) in 3" :mess="index"></shop-card>
-        </div>
-
-
+  <div class="box_col">
+    <div class="box_row rowRight seachSty">
+      <div>
+        <pager-tit title="商品"></pager-tit>
       </div>
+      <div class="box_row_100"></div>
+      <Input class="item" value="" placeholder="商品" style="width: 180px"/>
+      <Button class="item" type="primary" icon="ios-search"></Button>
+    </div>
+    <!--<div class="box_row rowBetween colCenter listTiT">-->
+      <!--<div style="width: 120px;">-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+        <!--<h2>商品</h2>-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+        <!--<h2>参与分数</h2>-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+        <!--<h2>剩余分数</h2>-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+        <!--<h2>商品状态</h2>-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+        <!--<h2>操作</h2>-->
+      <!--</div>-->
+    <!--</div>-->
+
+    <!--图  名字 参与分数 剩余分数 状态（售卖中 开奖中  已开奖） 详情-->
+    <div class="box_col_auto">
+      <div class="box_row_list" style="width: 1328px;margin: auto">
+        <shop-card :inVal="index" v-for="(it,index) in 5" :key="index"></shop-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import shopCard from "./comp/shopCard"
+  import shopCard from './comp/shopCard'
 
-    export default {
-
-        name: "index",
-        components:{
-          shopCard
-
-        }
+  export default {
+    name: "index",
+    components: {
+      shopCard
     }
+  }
 </script>
 
 <style lang="less">
-  .shopManager{
-    /*background-color:#eeeeee;*/
-    font-size: 0.18rem;
-
-    .barShow{
-      background-color: #f8f8f9;
-
-      color: #2d8cf0;
-      font-weight: 900;
+  .seachSty{
+    padding: 12px 0;
+    margin: 0 18px;
+    .item{
+      margin-left: 16px;
     }
-    .titleBarShow{
-      padding: 0 10px;
-      text-align: center;
-      height: 20px;
-
-    }
-
   }
-
+  .listTiT {
+    background-color: #ffba68;
+    color: #fff;
+    padding: 12px;
+    margin: 0 18px ;
+    text-align: center
+  }
 </style>
