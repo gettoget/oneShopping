ca2a3a60fbc007bccfbb382e2f8b4c57d098157f
youaretheappleinmyeyes <965567396@qq.com>
2019-05-25 21:54:10 +0800

    zhaohu

--- web/pc/src/locale/lang/en-US.js
+++ web/pc/src/locale/lang/en-US.js
@@ -1,7 +1,18 @@
 export default {
+  /**
+   * Menu i18n
+   * 菜单
+   */
+
+
+
+  /**
+   * pubilc i18n
+   * 公用
+   */
   home: 'Home',
   login: 'Login',
 
-  "NUMBER":"Number",
-  "SYSTEM_USER":"System user"
+  "NUMBER": "Number",
+  "SYSTEM_USER": "System user"
 }
--- web/pc/src/locale/lang/zh-CN.js
+++ web/pc/src/locale/lang/zh-CN.js
@@ -1,4 +1,16 @@
 export default {
+  /**
+   * Menu i18n
+   * 菜单
+   */
+  "system":"系统管理",
+  "system-user":"用户管理",
+
+
+  /**
+   * pubilc i18n
+   * 公用
+   */
   home:'首页',
   login:'登录',
 
