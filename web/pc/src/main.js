b8ccb087df332093cd556cbec0ce35214e4c7911
quan <1538452228@qq.com>
2019-06-10 23:39:47 +0800

    12

--- web/pc/src/main.js
+++ web/pc/src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-//  // "@vue/cli-plugin-eslint": "^3.0.1",语法检测
+//  "@vue/cli-plugin-eslint": "^3.0.1",语法检测
 import Vue from 'vue'
 import App from './App'
 import router from './router'
