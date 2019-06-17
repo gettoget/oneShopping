deb71e9d2ca1d21fa5ee5fc1a7e0ce1a9a659bc8
youaretheappleinmyeyes <965567396@qq.com>
2019-06-17 20:54:11 +0800

    zhaohu

--- web/pc/src/vShop/topUpData/comp/chartTopUp.vue
+++ web/pc/src/vShop/topUpData/comp/chartTopUp.vue
@@ -125,7 +125,7 @@
               selectedMode: false
             }
           ],
-          color: [ "#6495ed", "#ff69b4", "#ba55d3", "#cd5c5c", "#ffa500", "#40e0d0", "#1e90ff", "#ff6347", "#7b68ee", "#00fa9a", "#ffd700", "#6699FF", "#ff6666", "#3cb371", "#b8860b", "#30e0e0"]
+          color: [ "rgb(136, 173, 218)", "rgb(239, 157, 119)", "rgb(153, 206, 152)", "#cd5c5c", "#ffa500", "#40e0d0", "#1e90ff", "#ff6347", "#7b68ee", "#00fa9a", "#ffd700", "#6699FF", "#ff6666", "#3cb371", "#b8860b", "#30e0e0"]
         }
         myChart.setOption(option);
       }
