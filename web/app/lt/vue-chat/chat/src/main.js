51dfa406cedbbab4a25695937df9997d444de792
chenwei <chenwei@gongyingshi.com>
2019-06-23 19:15:10 +0800

    chat

---
+++ web/app/lt/vue-chat/chat/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+Vue.use(Mint);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
