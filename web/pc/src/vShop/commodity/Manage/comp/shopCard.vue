796628f32d37d46ddeb9cff6d43bb483cefa3e51
youaretheappleinmyeyes <965567396@qq.com>
2019-07-10 00:14:30 +0800

    z

--- web/pc/src/vShop/commodity/Manage/comp/shopCard.vue
+++ web/pc/src/vShop/commodity/Manage/comp/shopCard.vue
@@ -14,24 +14,30 @@
     <div class="shopMoney">
       ￥{{mess.proPrice}}
     </div>
-    <Poptip trigger="hover">
-      <div class="shopTit"  @click="getMess">
-        {{mess.proName}}
-      </div>
-      <div slot="content" style="width: 240px;white-space:normal;font-size: 18px">
-        {{mess.proName}}
-      </div>
-    </Poptip>
+    <div style="height: 60px">
+      <Poptip trigger="hover">
+        <div class="shopTit" @click="getMess">
+          {{mess.proName}}
+        </div>
+        <div slot="content" style="width: 240px;white-space:normal;font-size: 18px">
+          {{mess.proName}}
+        </div>
+      </Poptip>
+    </div>
     <div class="lineBox">
       <div class="lineVal">
         {{$t('YS')}}:{{(parseInt(mess.proPrice)-parseInt(mess.rePrice))}}
-        {{$t('F')}}/{{$t('SY')}}{{mess.rePrice}}{{$t('F')}}</div>
-      <div class="lineback box_row">
-        <div class="line" :style="{width:((parseInt(mess.proPrice)-parseInt(mess.rePrice))/parseInt(mess.rePrice))*100+'%'}">
-        </div>
-        <div class="box_row_100">
-        </div>
+        {{$t('F')}}/{{$t('SY')}}{{mess.rePrice}}{{$t('F')}}
       </div>
+      <Progress :percent="100-parseInt(parseInt(mess.rePrice)/parseInt(mess.proPrice)*100)" status="active">
+        <span v-if="mess.rePrice == 0">售罄</span>
+      </Progress>
+      <!--<div class="lineback box_row">-->
+      <!--<div class="line" :style="{width:((parseInt(mess.proPrice)-parseInt(mess.rePrice))/parseInt(mess.rePrice))*100+'%'}">-->
+      <!--</div>-->
+      <!--<div class="box_row_100">-->
+      <!--</div>-->
+      <!--</div>-->
     </div>
     <div class="shopBq">
       <!--新品、热门、喜欢，-->
@@ -42,9 +48,9 @@
       </Tag>
 
       <!--<Tag :color="'red'"-->
-           <!--v-for="(it,index) in mess.proLx.split(',')"-->
-           <!--@click.native="tagVal = index">-->
-        <!--{{it | TagVal}}-->
+      <!--v-for="(it,index) in mess.proLx.split(',')"-->
+      <!--@click.native="tagVal = index">-->
+      <!--{{it | TagVal}}-->
       <!--</Tag>-->
     </div>
   </Card>
@@ -53,6 +59,7 @@
 <script>
   import iphone from '../img/iphone.jpg'
   import i18nTabTit from '@/mixins/i18nTabTit'
+
   export default {
     name: "shopCard",
     mixins: [i18nTabTit],
@@ -67,7 +74,7 @@
       }
     },
     filters: {
-      proZt:(val)=>{
+      proZt: (val) => {
         switch (val) {
           case "1":
             return "售卖中"
@@ -107,72 +114,73 @@
       return {
         iphone,
         tagVal: 1,
-        tegList:[
+        tegList: [
           {
-            bol:false,
-            text:"推荐",
-            key:"1"
+            bol: false,
+            text: "推荐",
+            key: "1"
           },
           {
-            bol:false,
-            text:"上新",
-            key:"2"
+            bol: false,
+            text: "上新",
+            key: "2"
           },
           {
-            bol:false,
-            text:"热门",
-            key:"3"
+            bol: false,
+            text: "热门",
+            key: "3"
           },
         ]
       }
     },
-    created(){
-      this.tegList=[
+    created() {
+      this.tegList = [
         {
-          bol:false,
-          text:"推荐",
-          key:"1"
+          bol: false,
+          text: "推荐",
+          key: "1"
         },
         {
-          bol:false,
-          text:"上新",
-          key:"2"
+          bol: false,
+          text: "上新",
+          key: "2"
         },
         {
-          bol:false,
-          text:"热门",
-          key:"3"
+          bol: false,
+          text: "热门",
+          key: "3"
         },
       ]
       let a = this.mess.proLx.split(',')
-      a.forEach((it,index)=>{
-        this.tegList[parseInt(it)-1].bol = true
+      a.forEach((it, index) => {
+        this.tegList[parseInt(it) - 1].bol = true
       })
     },
-    mounted(){
+    mounted() {
     },
-    methods:{
-      getMess(){
+    methods: {
+      getMess() {
         this.$emit("getMess")
       },
-      tagEvent(it,index){
+      tagEvent(it, index) {
         it.bol = !it.bol
         let b = []
-        this.tegList.forEach((it,index)=>{
-          if(it.bol){
+        this.tegList.forEach((it, index) => {
+          if (it.bol) {
             b.push(it.key)
           }
-          if(index == this.tegList.length-1){
+          if (index == this.tegList.length - 1) {
             this.upShopMess(b.join(','))
           }
         })
       },
-      upShopMess(mes){
-        this.$http.post("/api/proinfo/update",{id:this.mess.id,proLx:mes}).then(res=>{
-          if(res.code == 200){
+      upShopMess(mes) {
+        this.$http.post("/api/proinfo/update", {id: this.mess.id, proLx: mes}).then(res => {
+          if (res.code == 200) {
             this.$emit('labSave')
           }
-        }).catch(err=>{})
+        }).catch(err => {
+        })
       }
     }
   }
@@ -196,7 +204,7 @@
         transform: rotate(-45deg);
         line-height: 30px;
         color: #fff;
-        text-align:center;
+        text-align: center;
       }
     }
     &-selImg {
