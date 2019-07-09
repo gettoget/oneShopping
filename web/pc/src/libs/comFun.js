e854e725c04a274f75787b55fcc871a29acd7e80
youaretheappleinmyeyes <965567396@qq.com>
2019-07-09 22:44:46 +0800

    a

--- web/pc/src/libs/comFun.js
+++ web/pc/src/libs/comFun.js
@@ -3,6 +3,17 @@ import { localRead,localSave, } from '@/libs/util'
 let Met={
 }
 
+Met.Format_Num = (s, n)=>{
+  n = n > 0 && n <= 20 ? n : 2;
+  s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+  var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
+  t = "";
+  for (i = 0; i < l.length; i++) {
+    t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+  }
+  return t.split("").reverse().join("") + "." + r;
+}
+
 Met.WindowListener = (callback)=>{
   // window.onresize = function(val) {
   //   // 浏览器窗口变化后需要做的事情
