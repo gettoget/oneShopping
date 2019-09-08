5b3169e84420fdfdc9664fb70bd8a33e5c90a412
youaretheappleinmyeyes <965567396@qq.com>
2019-09-08 16:12:59 +0800

    zhaohu

--- web/web_pay_text/src/App.vue
+++ web/web_pay_text/src/App.vue
@@ -1,23 +1,69 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="topbox">
+
+      HOW TO PAY
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/cimb.png             CIMB-->
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/danamon.png          Danamon-->
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/alfa.png             Alfa-->
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/permata.png          Permata-->
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/BNI.png              BNI-->
+        <!--https://wishwish.oss-ap-southeast-5.aliyuncs.com/payment/mandiri.png          Mandirifor="">-->
+    </div>
+    <div class="buttonBox">
+      <div class="bottonGroup box_row">
+        <div @click="fileIndex = 'BNI'"
+             class="buttonItem box_row_100" :class="fileIndex=='BNI'?'selBut':''">
+          BNI
+        </div>
+        <div @click="fileIndex = 'alfa'"
+             class="buttonItem box_row_100" :class="fileIndex=='alfa'?'selBut':''">
+          Alfamart
+        </div>
+        <div @click="fileIndex = 'cimb'"
+             class="buttonItem box_row_100" :class="fileIndex=='cimb'?'selBut':''">
+          CIMB NIAGA
+        </div>
+      </div>
+      <div class="bottonGroup box_row">
+        <div @click="fileIndex = 'mandiri'"
+             class="buttonItem box_row_100" :class="fileIndex=='mandiri'?'selBut':''">
+          mandiri
+        </div>
+        <div @click="fileIndex = 'permata'"
+             class="buttonItem box_row_100" :class="fileIndex=='permata'?'selBut':''">
+          PermataBank
+        </div>
+        <div @click="fileIndex = 'danamon'"
+             class="buttonItem box_row_100" :class="fileIndex=='danamon'?'selBut':''">
+          Danamon
+        </div>
+      </div>
+    </div>
+    <div class="imgFileBox">
+      <img v-if="fileIndex=='BNI'" src="./assets/img/bni.png" alt="">
+      <img v-else-if="fileIndex=='alfa'" src="./assets/img/alfa.png" alt="">
+      <img v-else-if="fileIndex=='cimb'" src="./assets/img/cimb.png" alt="">
+      <img v-else-if="fileIndex=='mandiri'" src="./assets/img/mandlrl.png" alt="">
+      <img v-else-if="fileIndex=='permata'" src="./assets/img/premata.png" alt="">
+      <img v-else-if="fileIndex=='danamon'" src="./assets/img/danamon.png" alt="">
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      fileIndex:'BNI'
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  @import "./assets/css/box.less";
+  @import "./app.less";
 </style>
