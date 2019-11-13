e21157bd09e47ee4e5e074ff4718614195d2440a
slu <384920400@qq.com>
2019-11-13 17:08:55 +0800

    update

--- biz/src/main/java/com/ldz/biz/controller/PopularImgContrl.java
+++ biz/src/main/java/com/ldz/biz/controller/PopularImgContrl.java
@@ -6,6 +6,7 @@ import com.ldz.sys.base.BaseController;
 import com.ldz.sys.base.BaseService;
 import com.ldz.util.bean.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,17 @@ public class PopularImgContrl extends BaseController<Popularimgs,String> {
     protected BaseService<Popularimgs, String> getBaseService() {
         return service;
     }
-
+    /**
+     * 数据删除方法
+     * 如果对数据要求高，请重写该方法或是不直接继承该类，防止数据泄露
+     * @param id
+     * @return
+     */
+    @RequestMapping(value="/remove/{pkid}", method={RequestMethod.POST})
+    public ApiResponse<String> remove(@PathVariable("pkid")String id){
+        service.removeEntity(id);
+        return ApiResponse.success();
+    }
     /**
      * 保存推荐轮播图
      * @param entity
